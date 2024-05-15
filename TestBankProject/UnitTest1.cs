ba93c747138d9e11de7cf1b6150789b9a797c2ed
Budaházi Bence <budahazi.bence@petrik.hu>
2024-05-15 13:23:39 +0200

    test: Add test project with example test file

---
+++ TestBankProject/UnitTest1.cs
@@ -0,0 +1,30 @@
+//namespace TestBankProject
+//{
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
+
+//        [Test]
+//        public void Test1()
+//        {
+//            // Sikeres teszt pipával jelölve
+//            Assert.Pass();
+//        }
+
+//        [Test]
+//        public void Test2()
+//        {
+//            // Sikertelen teszt piros X-el jelölve
+//            Assert.Fail();
+//        }
+
+//        public void Test3()
+//        {
+//            // Nem jelenik meg a testexplorer-en mert nincs megjelölve tesztként
+//            Assert.Pass();
+//        }
+//    }
+//}
