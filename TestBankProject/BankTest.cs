fcdd3aebc6096ab6d327fc2c346a88d5eff5e8b5
Budaházi Bence <budahazi.bence@petrik.hu>
2024-05-29 13:02:58 +0200

    test: Make bank object class variable in test file

--- TestBankProject/BankTest.cs
+++ TestBankProject/BankTest.cs
@@ -9,11 +9,18 @@ namespace TestBankProject
 {
     internal class BankTest
     {
+        Bank bank;
+
+        [SetUp]
+        public void Setup()
+        {
+            bank = new Bank();
+        }
+
         [Test]
         // FvNeve_MilyenEsetben_MitCsinál
         public void UjSzamla_HelyesAdatokkal_Egyenleg0Ft()
         {
-            Bank bank = new Bank();
             
             bank.UjSzamla("Gipsz Jakab", "1234");
 
@@ -23,40 +30,30 @@ namespace TestBankProject
         [Test]
         public void UjSzamla_NullNevvel_ArgumentNullExceptiontDob()
         {
-            Bank bank = new Bank();
-
             Assert.Throws<ArgumentNullException>(() => bank.UjSzamla(null, "1234"));
         }
 
         [Test]
         public void UjSzamla_UresNevvel_ArgumentExceptiontDob()
         {
-            Bank bank = new Bank();
-
             Assert.Throws<ArgumentException>(() => bank.UjSzamla("", "1234"));
         }
 
         [Test]
         public void UjSzamla_NullSzamlaszammal_ArgumentNullExceptiontDob()
         {
-            Bank bank = new Bank();
-
             Assert.Throws<ArgumentNullException>(() => bank.UjSzamla("Gipsz Jakab", null));
         }
 
         [Test]
         public void UjSzamla_UresSzamlaszammal_ArgumentExceptiontDob()
         {
-            Bank bank = new Bank();
-
             Assert.Throws<ArgumentException>(() => bank.UjSzamla("Gipsz Jakab", ""));
         }
 
         [Test]
         public void UjSzamla_LetezoSzamlaszammal_ArgumentExceptiontDob()
         {
-            Bank bank = new Bank();
-
             bank.UjSzamla("Gipsz Jakab", "1234");
 
             Assert.Throws<ArgumentException>(() => bank.UjSzamla("Teszt Elek", "1234"));
@@ -66,11 +63,11 @@ namespace TestBankProject
         [Test]
         public void UjSzamla_LetezoNevvel_NemDobExceptiont()
         {
-            Bank bank = new Bank();
-
             bank.UjSzamla("Gipsz Jakab", "1234");
 
             Assert.DoesNotThrow(() => bank.UjSzamla("Gipsz Jakab", "4321"));
         }
+
+
     }
 }
